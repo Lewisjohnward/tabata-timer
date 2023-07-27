48388a81402595dbbff0a7f64ccf0784b4f6b2e0
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-07-27 13:24:08 +0100

    removed overlay preventing buttons

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -103,23 +103,21 @@ const AddSequence = ({setView} : HomeProps) => {
     }, [open])
 
     return (
-        <div 
-            className={clsx("absolute h-[90vh] top-0 w-full text-white font-bold")}
-        >
+        <> 
             {open &&
             <div 
-                className={clsx("h-[2000px] bg-white/40")} 
+                className={clsx("absolute h-[2000px] w-full top-0 left-0 bg-white/40")} 
                 onClick={() => setOpen(false)}
             />
             }
-            <div className="fixed right-0 bottom-2 space-y-2">
+            <div className="fixed right-0 bottom-2 space-y-2 text-white text-2xl">
                 { open &&
-                <div className="flex justify-end items-center gap-4 pr-8 [&>*]:active:bg-white [&>*]:active:transition-colors [&>*]:active:duration-200">
+                <div className="flex justify-end items-center gap-4 pr-6 [&>*]:active:bg-white [&>*]:active:transition-colors [&>*]:active:duration-200">
                     <button className={clsx("py-1 px-4 bg-red-500 rounded-full shadow-[1px_2px_2px_0px_rgba(0,0,0,0.2)] animate-entrance")}>
                         New Sequence
                     </button>
                     <button 
-                        className={clsx("flex justify-center items-center w-10 h-10 bg-red-500 rounded-full text-2xl shadow-[1px_2px_2px_0px_rgba(0,0,0,0.2)] animate-riseup")}
+                        className={clsx("flex justify-center items-center w-14 h-14 bg-red-500 rounded-full text-3xl shadow-[1px_2px_2px_0px_rgba(0,0,0,0.2)] animate-riseup")}
                     >
                         <AiOutlineOrderedList />
                     </button>
@@ -128,7 +126,7 @@ const AddSequence = ({setView} : HomeProps) => {
                 <div className={clsx("flex justify-end items-center gap-4 [&>*]:active:bg-white [&>*]:active:transition-colors [&>*]:active:duration-200 pr-[10px]")}>
                     { open &&
                     <button 
-                        className="py-1 px-4 bg-red-500 rounded-full shadow-[1px_2px_2px_0px_rgba(0,0,0,0.2)] animate-entrance"
+                        className={clsx("py-1 px-4 bg-red-500 rounded-full shadow-[1px_2px_2px_0px_rgba(0,0,0,0.2)] animate-entrance")}
                         onClick={() => setView("addworkout")}
                     >
                         New workout
@@ -142,7 +140,7 @@ const AddSequence = ({setView} : HomeProps) => {
                     </button>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
