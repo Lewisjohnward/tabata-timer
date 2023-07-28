3e978dd5c47d43ccc586f0c6a1ea10c632d2c918
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-07-28 14:13:32 +0100

    seperated text input and number input

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -2,16 +2,15 @@ import clsx from 'clsx'
 import {ReactNode} from 'react'
 import { AiFillMinusCircle, AiFillPlusCircle } from 'react-icons/ai'
 
-type InputProps = {
+type NumberInputProps = {
     inputType: string,
     icon: ReactNode,
     label: string,
-    placeholder: string,
-    value: string,
-    setValue: React.Dispatch<React.SetStateAction<string>>
+    value: number,
+    setValue: React.Dispatch<React.SetStateAction<number>>
 }
 
-const Input = ({inputType, icon, label, placeholder, value, setValue} : InputProps) => {
+const NumberInput = ({inputType, icon, label, value, setValue} : NumberInputProps) => {
     return (
         <div className="flex">
             <div className="flex justify-center items-center text-6xl pl-2">
@@ -21,15 +20,16 @@ const Input = ({inputType, icon, label, placeholder, value, setValue} : InputPro
                 <label className="block font-bold">{label}</label>
                 <div className="flex justify-between items-center">
                     {inputType == "number" && 
-                    <button>
+                    <button
+                        onClick={() => setValue(prev => prev - 1)}
+                    >
                         <AiFillMinusCircle className="text-5xl" />
                     </button>
                     }
                     <input 
                         className={clsx("w-full text-center bg-transparent focus:outline-none text-2xl", inputType == "text" && "pb-4")} type={inputType} 
-                        placeholder={placeholder}
                         value={value}
-                        onChange={e =>  setValue(e.target.value)}
+                        onChange={e =>  setValue(Number(e.target.value))}
                     />
 
                     {inputType == "number" && 
@@ -44,4 +44,34 @@ const Input = ({inputType, icon, label, placeholder, value, setValue} : InputPro
     )
 }
 
-export default Input
+type TextInputProps = {
+    inputType: string,
+    icon: ReactNode,
+    label: string,
+    value: string,
+    setValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const TextInput = ({inputType, icon, label, value, setValue} : TextInputProps) => {
+    return (
+        <div className="flex">
+            <div className="flex justify-center items-center text-6xl pl-2">
+                {icon}
+            </div>
+            <div className="flex-grow ml-4 space-y-2 text-center border-b-[1px] border-black pb-2">
+                <label className="block font-bold">{label}</label>
+                <div className="flex justify-between items-center">
+                    <input 
+                        className={clsx("w-full text-center bg-transparent focus:outline-none text-2xl", inputType == "text" && "pb-4")} type={inputType} 
+                        value={value}
+                        onChange={e =>  setValue(e.target.value)}
+                    />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+
+export default NumberInput
