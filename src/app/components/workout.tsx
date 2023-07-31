253995c87d34b037fd08fbaeab61bda00443a5bf
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-07-31 11:43:31 +0100

    header style

--- src/app/components/workout.tsx
+++ src/app/components/workout.tsx
@@ -9,7 +9,7 @@ const Workout = () => {
   const bg = "green";
   return (
     <div className={clsx("h-screen text-white", `bg-[${bg}]`)}>
-      <div className="flex justify-center items-center gap-8 text-6xl font-bold">
+      <div className="flex justify-center items-center gap-8 text-8xl font-bold">
         <button onClick={() => setLocked((prev) => !prev)}>
           {locked ? <AiFillLock /> : <AiFillUnlock />}
         </button>
