be10d188bd1edb0d2def2b5ece21db7de401ce5e
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-07-31 13:03:41 +0100

    workout component

--- src/app/components/workout.tsx
+++ src/app/components/workout.tsx
@@ -1,22 +1,80 @@
 "use client";
+import clsx from "clsx";
 import { useState } from "react";
 import { AiFillLock, AiFillUnlock } from "react-icons/ai";
 import { BsFillPlayFill } from "react-icons/bs";
+import { HomeProps } from "./types";
 
-const Workout = () => {
+type Workout = {
+  prepare?: number;
+  work?: number;
+  rest?: number;
+  cooldown?: number;
+}[];
+
+const workout: Workout = [
+  { prepare: 10 },
+  { work: 25 },
+  { rest: 60 },
+  { work: 60 },
+  { rest: 25 },
+  { work: 60 },
+  { rest: 25 },
+  { work: 60 },
+  { rest: 25 },
+  { work: 60 },
+  { rest: 25 },
+  { work: 60 },
+  { rest: 25 },
+  { work: 60 },
+  { rest: 25 },
+  { cooldown: 5 },
+];
+
+const Workout = ({ setView }: HomeProps) => {
   const [locked, setLocked] = useState(false);
   const bg = "green";
   return (
-    <div className="h-screen text-white" style={{ backgroundColor: `${bg}` }}>
-      <div className="flex justify-center items-center gap-8 text-8xl font-bold">
-        <button onClick={() => setLocked((prev) => !prev)}>
-          {locked ? <AiFillLock /> : <AiFillUnlock />}
-        </button>
-        <h1 className="">Prepare</h1>
-        <button>
-          <BsFillPlayFill />
-        </button>
+    <div
+      className="relativeh-screen p-4 text-white pt-4 space-y-4"
+      style={{ backgroundColor: `${bg}` }}
+    >
+      <div className="space-y-10">
+        <div className="flex justify-center items-center gap-8 text-6xl font-bold">
+          <button onClick={() => setLocked((prev) => !prev)}>
+            {locked ? <AiFillLock /> : <AiFillUnlock />}
+          </button>
+          <h1 className="">Prepare</h1>
+          <button>
+            <BsFillPlayFill />
+          </button>
+        </div>
+        <div className="text-center text-[15rem] leading-none">10</div>
       </div>
+      <div>
+        {workout.map((d, i) => {
+          const key = Object.keys(d)[0];
+          const value = d[key as keyof typeof d];
+          return (
+            <div
+              className={clsx(
+                "border-b-[1px] border-white text-center text-4xl",
+                i == 0 && "bg-green-800 rounded"
+              )}
+            >
+              <button className="py-2 w-full rounded hover:bg-green-700">
+                {i + 1}. {key}: {value}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <button
+        className="absolute top-0 right-10 w-14 h-14 bg-white text-black rounded-lg shadow hover:bg-gray-200"
+        onClick={() => setView("home")}
+      >
+        Home
+      </button>
     </div>
   );
 };
