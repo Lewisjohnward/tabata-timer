54951ea5154d274b3fd30fd4d7e31428d2240039
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-07-31 11:39:53 +0100

    moved workout into seperate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-import clsx from "clsx";
 import { useState } from "react";
 import AddWorkout from "./components/addWorkout";
 import Home from "./components/home";
-import { AiFillLock, AiFillUnlock } from "react-icons/ai";
-import { BsFillPlayFill } from "react-icons/bs";
+import Workout from "./components/workout";
 
 export default function Page() {
   const [view, setView] = useState("home");
@@ -21,21 +19,3 @@ export default function Page() {
     </main>
   );
 }
-
-const Workout = () => {
-  const [locked, setLocked] = useState(false);
-  const bg = "green";
-  return (
-    <div className={clsx("h-screen text-white", `bg-[${bg}]`)}>
-      <div className="flex justify-center items-center gap-8 text-6xl font-bold">
-        <button onClick={() => setLocked((prev) => !prev)}>
-          {locked ? <AiFillLock /> : <AiFillUnlock />}
-        </button>
-        <h1 className="">Prepare</h1>
-        <button>
-          <BsFillPlayFill />
-        </button>
-      </div>
-    </div>
-  );
-};
