06d63ca371fb5179a39f785653713a72b23bcd71
lewis <lewisjohnward+git1@gmail.com>
2023-08-02 23:45:44 +0100

    removed summarytype

--- src/app/components/types.ts
+++ src/app/components/types.ts
@@ -1,15 +1,3 @@
 export type HomeProps = {
     setView: React.Dispatch<React.SetStateAction<string>>
 }
-
-
-export type SummaryType = {
-  set?: number;
-  cycle?: number;
-  prepare?: number;
-  work?: number;
-  rest?: number;
-  restBetweenSets?: number;
-  cooldown?: number;
-};
-
