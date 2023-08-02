fd0b0273c0b892d210f24e6e4bc53f99f8350000
lewis <lewisjohnward+git1@gmail.com>
2023-08-02 23:52:56 +0100

    removed workout component into seperate file

---
+++ src/app/components/workout.tsx
@@ -0,0 +1,45 @@
+import {SetStateAction} from "react";
+import {BsFillPlayFill} from "react-icons/bs";
+import {FaEllipsisV} from "react-icons/fa";
+import {Workout as WorkoutType} from "../types/Workout";
+
+type Props = {
+    setView: React.Dispatch<SetStateAction<string>>;
+    setActiveWorkout: React.Dispatch<SetStateAction<WorkoutType | undefined>>;
+    workout: WorkoutType;
+};
+
+const Workout = ({setView, workout, setActiveWorkout}: Props) => {
+    const openMenu = () => {
+        console.log("open menu");
+    };
+
+    const handleActivateWorkout = () => {
+        setView("workout");
+        setActiveWorkout(workout);
+    };
+
+    return (
+        <div
+            className="flex space-between p-4 bg-red-500 text-white shadow-[4px_2px_2px_0px_rgba(0,0,0,0.2)]"
+            style={{backgroundColor: `${workout.color}`}}
+        >
+            <div>
+                <h3 className="font-bold text-xl">{workout.title}</h3>
+                <p className="text-lg">
+                    {workout.totalTime} {workout.intervals} intervals
+                </p>
+            </div>
+            <div className="flex-grow flex justify-end gap-4 text-2xl [&>button]:text-4xl">
+                <button>
+                    <BsFillPlayFill onClick={handleActivateWorkout} />
+                </button>
+                <button>
+                    <FaEllipsisV onClick={openMenu} />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Workout
--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,72 +1,32 @@
-import { SetStateAction } from "react";
-import { BsFillPlayFill } from "react-icons/bs";
-import { FaEllipsisV } from "react-icons/fa";
+import {SetStateAction} from "react";
 import AddIcon from "../components/addIcon";
 import Header from "../components/header";
-import { Workout as WorkoutType } from "../types/Workout";
+import Workout from "../components/workout";
+import {Workout as WorkoutType} from "../types/Workout";
 
 type HomeProps = {
-  setView: React.Dispatch<SetStateAction<string>>;
-  setActiveWorkout: React.Dispatch<SetStateAction<WorkoutType | undefined>>;
-  workouts: WorkoutType[];
+    setView: React.Dispatch<SetStateAction<string>>;
+    setActiveWorkout: React.Dispatch<SetStateAction<WorkoutType | undefined>>;
+    workouts: WorkoutType[];
 };
 
-const Home = ({ setView, workouts, setActiveWorkout }: HomeProps) => {
-  return (
-    <>
-      <Header />
-      <div className="py-2 space-y-2">
-        {workouts.map((workout) => (
-          <Workout
-            key={workout.id}
-            setView={setView}
-            workout={workout}
-            setActiveWorkout={setActiveWorkout}
-          />
-        ))}
-      </div>
-      <AddIcon setView={setView} />
-    </>
-  );
-};
-
-type Props = {
-  setView: React.Dispatch<SetStateAction<string>>;
-  setActiveWorkout: React.Dispatch<SetStateAction<WorkoutType | undefined>>;
-  workout: WorkoutType;
-};
-
-const Workout = ({ setView, workout, setActiveWorkout }: Props) => {
-  const openMenu = () => {
-    console.log("open menu");
-  };
-
-  const handleActivateWorkout = () => {
-    setView("workout");
-    setActiveWorkout(workout);
-  };
-
-  return (
-    <div
-      className="flex space-between p-4 bg-red-500 text-white shadow-[4px_2px_2px_0px_rgba(0,0,0,0.2)]"
-      style={{ backgroundColor: `${workout.color}` }}
-    >
-      <div>
-        <h3 className="font-bold text-xl">{workout.title}</h3>
-        <p className="text-lg">
-          {workout.totalTime} {workout.intervals} intervals
-        </p>
-      </div>
-      <div className="flex-grow flex justify-end gap-4 text-2xl [&>button]:text-4xl">
-        <button>
-          <BsFillPlayFill onClick={handleActivateWorkout} />
-        </button>
-        <button>
-          <FaEllipsisV onClick={openMenu} />
-        </button>
-      </div>
-    </div>
-  );
+const Home = ({setView, workouts, setActiveWorkout}: HomeProps) => {
+    return (
+        <>
+            <Header />
+            <div className="py-2 space-y-2">
+                {workouts.map((workout) => (
+                    <Workout
+                        key={workout.id}
+                        setView={setView}
+                        workout={workout}
+                        setActiveWorkout={setActiveWorkout}
+                    />
+                ))}
+            </div>
+            <AddIcon setView={setView} />
+        </>
+    );
 };
 
 export default Home;
