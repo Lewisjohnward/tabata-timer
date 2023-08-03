b254a4572084f5f6ae2d3abd8f5f1eebc26d87d6
lewis <lewisjohnward+git1@gmail.com>
2023-08-03 23:20:20 +0100

    formatted time

--- src/app/components/workout.tsx
+++ src/app/components/workout.tsx
@@ -2,6 +2,7 @@ import {SetStateAction} from "react";
 import {BsFillPlayFill} from "react-icons/bs";
 import {FaEllipsisV} from "react-icons/fa";
 import {Workout as WorkoutType} from "../types/Workout";
+import convertTime from "../helpers/convertTime"
 
 type Props = {
     setView: React.Dispatch<SetStateAction<string>>;
@@ -19,6 +20,7 @@ const Workout = ({setView, workout, setActiveWorkout}: Props) => {
         setActiveWorkout(workout);
     };
 
+
     return (
         <div
             className="flex space-between p-4 bg-red-500 text-white shadow-[4px_2px_2px_0px_rgba(0,0,0,0.2)]"
@@ -27,7 +29,7 @@ const Workout = ({setView, workout, setActiveWorkout}: Props) => {
             <div>
                 <h3 className="font-bold text-xl">{workout.title}</h3>
                 <p className="text-lg">
-                    {workout.totalTime} {workout.intervals} intervals
+                    {convertTime(workout.totalTime)} {workout.intervals} intervals
                 </p>
             </div>
             <div className="flex-grow flex justify-end gap-4 text-2xl [&>button]:text-4xl">
