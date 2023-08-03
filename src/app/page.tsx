191f7c921af8081305286334bb3309c10e2c709a
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-08-03 16:11:22 +0100

    indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,28 @@
 "use client";
-import {useState} from "react";
+import { useState } from "react";
 import ActiveWorkout from "./pages/activeWorkout";
 import AddWorkout from "./pages/addWorkout";
 import Home from "./pages/home";
-import {Workout} from "./types/Workout";
+import { Workout } from "./types/Workout";
 
 export default function Page() {
-    const [view, setView] = useState("home");
-    const [workouts, setWorkouts] = useState<never | Workout[]>([]);
-    const [activeWorkout, setActiveWorkout] = useState<Workout>();
+  const [view, setView] = useState("home");
+  const [workouts, setWorkouts] = useState<never | Workout[]>([]);
+  const [activeWorkout, setActiveWorkout] = useState<Workout>();
 
-    return (
-        <main className="relative min-h-full">
-            {view == "home" ? (
-                <Home
-                    setView={setView}
-                    workouts={workouts}
-                    setActiveWorkout={setActiveWorkout}
-                />
-            ) : view == "addworkout" ? (
-                <AddWorkout setView={setView} setWorkouts={setWorkouts} />
-            ) : view == "workout" ? (
-                <ActiveWorkout setView={setView} activeWorkout={activeWorkout} />
-            ) : null}
-        </main>
-    );
+  return (
+    <main className="relative min-h-full">
+      {view == "home" ? (
+        <Home
+          setView={setView}
+          workouts={workouts}
+          setActiveWorkout={setActiveWorkout}
+        />
+      ) : view == "addworkout" ? (
+        <AddWorkout setView={setView} setWorkouts={setWorkouts} />
+      ) : view == "workout" ? (
+        <ActiveWorkout setView={setView} activeWorkout={activeWorkout} />
+      ) : null}
+    </main>
+  );
 }
