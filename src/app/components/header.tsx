dc9ed1b8f761750b5e889b85b62f752e5d81619d
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-08-04 19:19:17 +0100

    separated Button component

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ReactNode, SetStateAction, useState } from "react";
-import { IconType } from "react-icons";
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
 import {
   BsArrowsCollapse,
   BsArrowsExpand,
@@ -10,11 +9,7 @@ import {
   BsStarFill,
 } from "react-icons/bs";
 
-import { CiSettings } from "react-icons/ci";
-import { FaExpand } from "react-icons/fa";
-import { MdExpandLess, MdExpandMore } from "react-icons/md";
-
-const IconArr = [<AiOutlineSearch />, <MdExpandLess />, <CiSettings />];
+import { MdExpandLess, MdExpandMore, MdSettings } from "react-icons/md";
 
 type Props = {
   workoutCount: number;
@@ -26,35 +21,35 @@ const Header = ({ workoutCount }: Props) => {
   const [expandedWorkouts, setExpandedWorkouts] = useState(false);
   return (
     <>
-      <div className="flex justify-between gap-4 bg-gray-400 px-4 py-2 text-white font-bold">
+      <div className="flex justify-between gap-4 bg-gray-400 px-4 py-4 text-white font-bold">
         <div className="space-y-4">
           <h1 className="text-2xl">Workouts: {workoutCount}</h1>
         </div>
         <div className="flex gap-6 text-2xl">
-          <button>
+          <Button>
             <AiOutlineSearch />
-          </button>
+          </Button>
           <Button onClickEvent={() => setOptionsVisible((prev) => !prev)}>
             {!optionsVisible ? <MdExpandMore /> : <MdExpandLess />}
           </Button>
-          <button>
-            <CiSettings />
-          </button>
+          <Button>
+            <MdSettings />
+          </Button>
         </div>
       </div>
       {optionsVisible && (
         <div className="flex justify-between px-4 py-2 bg-gray-400 text-white text-xl">
           <p>all</p>
           <div className="flex gap-6 text-2xl">
-            <button>
+            <Button>
               <BsPalette />
-            </button>
-            <button onClick={() => setSortFavorites((prev) => !prev)}>
+            </Button>
+            <Button onClickEvent={() => setSortFavorites((prev) => !prev)}>
               {sortFavorites ? <BsStarFill /> : <BsStar />}
-            </button>
-            <button onClick={() => setExpandedWorkouts((prev) => !prev)}>
+            </Button>
+            <Button onClickEvent={() => setExpandedWorkouts((prev) => !prev)}>
               {!expandedWorkouts ? <BsArrowsExpand /> : <BsArrowsCollapse />}
-            </button>
+            </Button>
           </div>
         </div>
       )}
@@ -67,9 +62,16 @@ const Button = ({
   onClickEvent,
 }: {
   children: ReactNode;
-  onClickEvent: React.Dispatch<SetStateAction<boolean>>;
+  onClickEvent?: React.Dispatch<SetStateAction<boolean>>;
 }) => {
-  return <button onClick={() => onClickEvent(true)}>{children}</button>;
+  return (
+    <button
+      className="hover:text-white/40"
+      onClick={() => onClickEvent != undefined && onClickEvent(true)}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Header;
