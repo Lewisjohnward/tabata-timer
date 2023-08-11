f3d2e4f1c6755cfb321ffea6f880ed172219f8b0
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-08-11 20:11:22 +0100

    generate summary

--- src/app/helpers/generateSummary.ts
+++ src/app/helpers/generateSummary.ts
@@ -1,7 +1,8 @@
 import calculateTotalTime from "./calculateTotalTime";
 import generateArray from "./generateArray";
+import convertTime from "./convertTime";
 
-const generateSummary = (intervalArr: {
+const generateSummary = (workout: {
   prepare: number;
   work: number;
   rest: number;
@@ -17,9 +18,64 @@ const generateSummary = (intervalArr: {
   //    "Rest: 01:10 . 7 intervals",
   //  ],
   //};
-  /* calculate total time */
-  //const totalTime = calculateTotalTime(arr);
-  //console.log(totalTime);
+  const { prepare, work, rest, cycles, sets, restBetweenSets, cooldown } =
+    workout;
+  const arr = [];
+  let totalTime = calculateTotalTime(
+    generateArray({
+      prepare,
+      work,
+      rest,
+      cycles,
+      sets,
+      restBetweenSets,
+      cooldown,
+    })
+  );
+  let interval = 1;
+
+  for (let set = 1; set <= sets; set++) {
+    sets > 1 && arr.push(`Set: ${set}`);
+
+    for (let cycle = 1; cycle <= cycles; cycle++) {
+      cycles > 1 && arr.push(`Cycle: ${cycle}`);
+
+      if (set == 1 && cycle == 1 && prepare != 0) {
+        arr.push(
+          `${interval++}.Prepare: ${prepare} - ${convertTime(totalTime)}`
+        );
+        totalTime -= prepare;
+      }
+
+      arr.push(`${interval++}.Work: ${work} - ${convertTime(totalTime)}`);
+      totalTime -= work;
+
+      if (
+        (cycle != cycles && rest != 0) ||
+        (cycle == cycles && set == sets && rest != 0)
+      ) {
+        arr.push(`${interval++}.Rest: ${rest} - ${convertTime(totalTime)}`);
+        totalTime -= rest;
+      }
+    }
+    if (restBetweenSets > 1 && set != sets) {
+      arr.push(
+        `${interval++}.Rest Between sets: ${restBetweenSets} - ${convertTime(
+          totalTime
+        )}`
+      );
+      totalTime -= restBetweenSets;
+    }
+  }
+
+  if (cooldown != 0) {
+    arr.push(`${interval++}.Cooldown: ${cooldown} - ${convertTime(totalTime)}`);
+    totalTime -= cooldown;
+  }
+  arr.push(`Finish: - ${convertTime(totalTime)}`);
+  console.log(arr);
+
+  return arr;
 };
 
 export default generateSummary;
