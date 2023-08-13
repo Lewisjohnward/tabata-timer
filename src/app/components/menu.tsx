fd3c3f797d4f77762aaeb4a3db672d4f975ee74e
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-08-14 00:34:40 +0100

    menu component for favouriting etc

---
+++ src/app/components/menu.tsx
@@ -0,0 +1,10 @@
+const Menu = () => {
+  return (
+    <div className="absolute top-10 right-10 bg-white w-96 h-96 shadow rounded-md text-black">
+      <p>hello this</p>
+      <p>hello this</p>
+    </div>
+  );
+};
+
+export default Menu;
