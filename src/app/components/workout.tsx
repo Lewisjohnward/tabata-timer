fb45ecd011246979fca035c5a0cf5b37bb7d1827
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-08-14 00:34:11 +0100

    added state for open close menu/ created div for absolute menu

--- src/app/components/workout.tsx
+++ src/app/components/workout.tsx
@@ -1,8 +1,11 @@
+"use client";
+import { useState } from "react";
 import { SetStateAction } from "react";
 import { BsFillPlayFill } from "react-icons/bs";
 import { FaEllipsisV } from "react-icons/fa";
 import { WorkoutObj } from "../types/WorkoutObj";
 import convertTime from "../helpers/convertTime";
+import Menu from "./menu";
 
 type Props = {
   setView: React.Dispatch<SetStateAction<string>>;
@@ -11,8 +14,10 @@ type Props = {
 };
 
 const Workout = ({ setView, workout, setActiveWorkout }: Props) => {
-  const openMenu = () => {
-    console.log("open menu");
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+    true;
   };
 
   const handleActivateWorkout = () => {
@@ -21,27 +26,32 @@ const Workout = ({ setView, workout, setActiveWorkout }: Props) => {
   };
 
   return (
-    <div
-      className="flex space-between p-4 text-white rounded-lg"
-      style={{ backgroundColor: `${workout.color}` }}
-    >
-      <div className="space-y-4">
-        <h3 className="font-bold text-xl">{workout.title}</h3>
-        <p className="text-xl">
-          {`Total: ${convertTime(workout.totalTime)} - ${
-            workout.intervals
-          } intervals`}
-        </p>
+    <>
+      <div
+        className="flex space-between p-4 text-white rounded-lg"
+        style={{ backgroundColor: `${workout.color}` }}
+      >
+        <div className="space-y-4">
+          <h3 className="font-bold text-xl">{workout.title}</h3>
+          <p className="text-xl">
+            {`Total: ${convertTime(workout.totalTime)} - ${
+              workout.intervals
+            } intervals`}
+          </p>
+        </div>
+        <div className="flex-grow flex justify-end gap-4 text-2xl [&>button]:text-4xl">
+          <button>
+            <BsFillPlayFill onClick={handleActivateWorkout} />
+          </button>
+          <div className="relative flex justify-center items-center">
+            <button>
+              <FaEllipsisV onClick={toggleMenu} />
+            </button>
+            {menuOpen && <Menu />}
+          </div>
+        </div>
       </div>
-      <div className="flex-grow flex justify-end gap-4 text-2xl [&>button]:text-4xl">
-        <button>
-          <BsFillPlayFill onClick={handleActivateWorkout} />
-        </button>
-        <button>
-          <FaEllipsisV onClick={openMenu} />
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
