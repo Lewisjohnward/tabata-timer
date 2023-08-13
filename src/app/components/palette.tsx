06b5e8315b6e68038e773911c34f179b1997a7df
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-08-13 18:49:50 +0100

    accepts color: string[] color: obj[] to display numbers of colors

--- src/app/components/palette.tsx
+++ src/app/components/palette.tsx
@@ -1,21 +1,24 @@
-import { colors } from "../misc/colors";
 import Modal from "./modal";
 import { TiTick } from "react-icons/ti";
 
 type PaletteProps = {
   setPaletteVisible: React.Dispatch<React.SetStateAction<boolean>>;
   setColor: React.Dispatch<React.SetStateAction<string>>;
+  colors: string[] | { color: string; number: number }[];
   selectedColor: string;
   closeOnSelect: boolean;
   displaySelection: boolean;
+  displayNumbers: boolean;
 };
 
 const Palette = ({
   setPaletteVisible,
   setColor,
+  colors,
   selectedColor,
   closeOnSelect,
   displaySelection,
+  displayNumbers,
 }: PaletteProps) => {
   const handleColorSelect = (
     event: React.MouseEvent<HTMLElement>,
@@ -34,18 +37,26 @@ const Palette = ({
       <div className="-mt-14 md:mt-0 bg-white rounded shadow p-4 space-y-10 md:p-8">
         <h2 className="text-xl font-bold">Select a color</h2>
         <div className="grid grid-cols-4 md:grid-cols-5 justify-items-center gap-4">
-          {colors.map((color) => (
-            <button
-              key={color}
-              className="w-20 h-20 rounded-full md:w-28 md:h-28"
-              style={{ backgroundColor: color }}
-              onClick={(e) => handleColorSelect(e, color)}
-            >
-              {selectedColor == color && displaySelection && (
-                <TiTick className="m-auto text-4xl md:text-6xl text-white bg-black/10 rounded-full" />
-              )}
-            </button>
-          ))}
+          {colors.map((color) => {
+            const _color = typeof color == "string" ? color : color.color;
+            return (
+              <button
+                key={_color}
+                className="w-20 h-20 rounded-full md:w-28 md:h-28"
+                style={{ backgroundColor: _color }}
+                onClick={(e) => handleColorSelect(e, _color)}
+              >
+                {displayNumbers && typeof color == "object" && (
+                  <p className="text-white font-bold text-xl md:text-4xl">
+                    {color.number}
+                  </p>
+                )}
+                {selectedColor == color && displaySelection && (
+                  <TiTick className="m-auto text-4xl md:text-6xl text-white bg-black/10 rounded-full" />
+                )}
+              </button>
+            );
+          })}
         </div>
       </div>
     </Modal>
