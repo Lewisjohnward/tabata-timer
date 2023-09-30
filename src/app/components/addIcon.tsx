d4baa0944823251599265b200590cae45df997c3
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-09-30 15:10:23 +0100

    changed styling mobile

--- src/app/components/addIcon.tsx
+++ src/app/components/addIcon.tsx
@@ -55,15 +55,13 @@ const AddIcon = ({ setView, user }: Props) => {
     <>
       {open && (
         <div
-          className={clsx(
-            "absolute h-[2000px] w-full top-0 left-0 bg-white/40"
-          )}
+          className={clsx("absolute h-full w-full top-0 left-0 bg-white/40")}
           onClick={() => setOpen(false)}
         />
       )}
-      <div className="fixed right-0 bottom-2 space-y-2 text-white text-2xl">
+      <div className="fixed right-1 bottom-1 md:bottom-2 space-y-2 text-white text-xl md:text-2xl">
         {open && (
-          <div className="flex justify-end items-center gap-4 pr-6 [&>*]:active:bg-white [&>*]:active:transition-colors [&>*]:active:duration-200">
+          <div className="flex justify-between items-center gap-4 [&>*]:active:bg-white [&>*]:active:transition-colors [&>*]:active:duration-200">
             <button
               className={clsx(
                 "py-1 px-4 bg-red-500 rounded-full shadow-[1px_2px_2px_0px_rgba(0,0,0,0.2)] animate-entrance"
@@ -83,7 +81,7 @@ const AddIcon = ({ setView, user }: Props) => {
         )}
         <div
           className={clsx(
-            "flex justify-end items-center gap-4 [&>*]:active:bg-white [&>*]:active:transition-colors [&>*]:active:duration-200 pr-[10px]"
+            "flex items-center gap-4 [&>*]:active:bg-white [&>*]:active:transition-colors [&>*]:active:duration-200"
           )}
         >
           {open && (
@@ -104,7 +102,7 @@ const AddIcon = ({ setView, user }: Props) => {
           >
             <div
               className={clsx(
-                "w-10 h-10 transition-transform duration-500 leading-none",
+                "w-10 h-10 transition-transform duration-300 leading-none",
                 open && "rotate-180"
               )}
             >
