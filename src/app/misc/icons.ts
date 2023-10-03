ed2fc577fef6d4f52934752b1b97c332b233844f
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-10-03 07:08:46 +0100

    updated

---
+++ src/app/misc/icons.ts
@@ -0,0 +1,19 @@
+import {
+  AiFillHome,
+  AiOutlineHome,
+  AiFillLock,
+  AiFillUnlock,
+} from "react-icons/ai";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { FaStepBackward, FaStepForward } from "react-icons/fa";
+
+export {
+  AiFillHome,
+  AiOutlineHome,
+  AiFillUnlock,
+  AiFillLock,
+  BsFillPlayFill,
+  BsFillPauseFill,
+  FaStepBackward,
+  FaStepForward,
+};
