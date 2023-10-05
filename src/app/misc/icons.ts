5257a7c9cea763a2c0735c314566958d8f4ec778
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-10-05 14:20:30 +0100

    added back arrow icon

--- src/app/misc/icons.ts
+++ src/app/misc/icons.ts
@@ -6,12 +6,14 @@ import {
 } from "react-icons/ai";
 import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 import { FaStepBackward, FaStepForward } from "react-icons/fa";
+import { BiArrowBack } from "react-icons/bi";
 
 export {
   AiFillHome,
   AiOutlineHome,
   AiFillUnlock,
   AiFillLock,
+  BiArrowBack,
   BsFillPlayFill,
   BsFillPauseFill,
   FaStepBackward,
