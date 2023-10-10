94d036dfa19010a97e64b35ee90d773dd5db2e06
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-10-10 19:54:39 +0100

    added icons

--- src/app/misc/icons.ts
+++ src/app/misc/icons.ts
@@ -1,4 +1,6 @@
 import {
+  AiOutlineClose,
+  AiFillEye,
   AiFillHome,
   AiOutlineSearch,
   AiOutlineHome,
@@ -12,12 +14,24 @@ import {
   BsArrowsExpand,
   BsPalette,
   BsPaletteFill,
+  BsFillPaletteFill,
+  BsArrowRepeat,
+  BsFillStopwatchFill,
+  BsSnow,
   BsStar,
   BsStarFill,
 } from "react-icons/bs";
-import { FaStepBackward, FaStepForward } from "react-icons/fa";
+import {
+  MdExpandLess,
+  MdExpandMore,
+  MdSettings,
+  MdTitle,
+} from "react-icons/md";
+import { FaStepBackward, FaStepForward, FaWalking } from "react-icons/fa";
 import { BiArrowBack } from "react-icons/bi";
-import { MdExpandLess, MdExpandMore, MdSettings } from "react-icons/md";
+import { GiWeightLiftingUp, GiSofa } from "react-icons/gi";
+import { RxSpaceBetweenHorizontally } from "react-icons/rx";
+import { TiTick } from "react-icons/ti";
 
 export {
   AiFillHome,
@@ -25,18 +39,30 @@ export {
   AiFillUnlock,
   AiFillLock,
   AiOutlineSearch,
+  AiOutlineClose,
+  AiFillEye,
   BiArrowBack,
   BsFillPlayFill,
   BsFillPauseFill,
+  BsArrowRepeat,
   BsArrowsCollapse,
   BsArrowsExpand,
+  BsFillStopwatchFill,
+  BsSnow,
   BsPalette,
   BsPaletteFill,
+  BsFillPaletteFill,
   BsStar,
   BsStarFill,
+  FaWalking,
+  GiWeightLiftingUp,
+  GiSofa,
   FaStepBackward,
   FaStepForward,
+  MdTitle,
   MdExpandLess,
   MdExpandMore,
   MdSettings,
+  RxSpaceBetweenHorizontally,
+  TiTick,
 };
