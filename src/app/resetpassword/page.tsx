eba3c814bfb50341eda022406fb9c2507a1d91d0
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-10-11 17:30:54 +0100

    added reset password page

---
+++ src/app/resetpassword/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { colors } from "@/misc/colors";
+import AuthLayout from "@/components/authLayout";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { FormEvent } from "react";
+
+const color = colors[Math.floor(Math.random() * colors.length)];
+
+interface FormData {
+  password: { value: string };
+}
+
+const Page = () => {
+  const supabase = createClientComponentClient();
+
+  const handleUpdatePassword = async (e: FormEvent<HTMLFormElement>) => {
+    console.log("reseting password");
+    const { password } = e.target as typeof e.target & FormData;
+    e.preventDefault();
+    const { error } = await supabase.auth.updateUser({
+      password: password.value,
+    });
+    console.log(error);
+  };
+
+  return (
+    <AuthLayout>
+      <h1 className="text-xl font-bold my-4 text-center">
+        Reset your password
+      </h1>
+      <p className="text-sm mb-4">
+        Tell us the email address associated with your Tabata Timer account, and
+        we’ll send you an email with a link to reset your password.
+      </p>
+      <form
+        className="flex-1 flex flex-col w-full justify-center gap-4 text-foreground [&>input]:outline-none"
+        onSubmit={handleUpdatePassword}
+      >
+        <input
+          className="rounded-md px-4 py-2 border"
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          required
+        />
+        <button
+          className="px-4 py-2 mb-2 text-white font-bold hover:bg-gray-500"
+          style={{ backgroundColor: color }}
+        >
+          Reset password
+        </button>
+      </form>
+    </AuthLayout>
+  );
+};
+
+export default Page;
