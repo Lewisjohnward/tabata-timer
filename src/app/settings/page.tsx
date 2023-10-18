3d2b21aeb8685e78d675cf16178aa9c2d4f0484c
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-10-18 12:51:07 +0100

    settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
+import { BiArrowBack } from "@/misc/icons";
+
 const Page = () => {
   return (
-    <div>
-      <div>settings</div>
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex items-center gap-4 justify-center w-full bg-red-50">
+        <Link href="/">
+          <BiArrowBack />
+        </Link>
+
+        <h1>Settings</h1>
+      </div>
+      <h2>Timer</h2>
+      <div className="bg-red-50">
+        <h2>Default values</h2>
+        <p>Prepare: 10 sec</p>
+        <p>Work: 20 sec</p>
+        <p>Rest: 10 sec</p>
+        <p>Cycles: 8</p>
+        <p>Sets: 8</p>
+        <p>Rest between sets: 10 sec</p>
+        <p>Cool down: 10 sec</p>
+      </div>
+      <div className="bg-gray-200 h-[1px] w-full"></div>
     </div>
   );
 };
